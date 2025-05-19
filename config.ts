4c61968fd20bfcec90a08afa146c51315af252a4
sumanta ghosh <sumantaghosh5333@gmail.com>
2025-05-19 10:40:10 +0530

    fix

--- config.ts
+++
@@ -1,6 +0,0 @@
-export const API_KEY = "AIzaSyDpsLC2bcTv9Bn8Gqtt89rnJCeyd8m_JYE";
-export const AUTH_DOMAIN = "chat-app-eb11c.firebaseapp.com";
-export const PROJECT_ID = "chat-app-eb11c";
-export const STORAGE_BUCKET = "chat-app-eb11c.appspot.com";
-export const MESSAGING_SENDER_ID = "452423416200";
-export const APP_ID = "1:452423416200:web:f0f792e976157671cae23d";
